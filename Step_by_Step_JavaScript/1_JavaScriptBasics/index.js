6c54b3e3f19b370eb0d6c05747f71d3ec7c05994
farnaz <farnaz.towhidi@gmail.com>
2025-04-02 14:55:59 -0400

    add example

--- Step_by_Step_JavaScript/1_JavaScriptBasics/index.js
+++ Step_by_Step_JavaScript/1_JavaScriptBasics/index.js
@@ -1,38 +1,17 @@
-console.log ("This is external JS")
+let myName = "Farnaz Towhidi"
 
-let message1 = "Hello, world!"
-console.log (typeof (message1))
-
-message1 = 12
-console.log (typeof (message1))
-
-message1 = true
-console.log (typeof (message1))
-
-let dayweek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
-let myNumbers = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,100]
-                //  0   1  2  3  4  
-console.log (dayweek[0])    
-console.log (dayweek[1]) 
-console.log (dayweek[6]) 
-console.log (dayweek.length)
-
-console.log (dayweek[dayweek.length-1]) 
-console.log (myNumbers[myNumbers.length-1]) 
-
-for (let i = 0; i < 10; i++) {
-  console.log (myNumbers[i])
-}
-
-console.log (myNumbers[4])
+console.log (myName)
+console.log ("This is me: " + myName)
+console.log (`This is me ${myName}`)
 
 
+let myAge = 49.5
+console.log (myAge)
 
-let myStudent = {
-  studentName: "Tom",
-  studentAge: 20,
-  isPassed: true
-}
 
-console.log (myStudent.isPassed)
+let isMarried = true
+
+console.log (typeof (myName))
+console.log (typeof (myAge))
+console.log (typeof (myAge))
 
