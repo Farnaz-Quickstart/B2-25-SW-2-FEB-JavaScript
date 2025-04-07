6261a468d73ca343179d4a718748e36bb60e4fdb
farnaz <farnaz.towhidi@gmail.com>
2025-04-07 15:17:13 -0400

    Update for

--- Step_by_Step_JavaScript/4_loops/script/loopFor.js
+++ Step_by_Step_JavaScript/4_loops/script/loopFor.js
@@ -1,27 +1,16 @@
+// For loop: Repeat some code a LIMITED amount of time
 // Reference: https://www.w3schools.com/js/js_loop_for.asp
 // for (initialization; condition ; condition) {}
 
 
-
-
-// for (let i = 0 ;  i < 5 ; i++) {
-//   console.log (i)
-//   console.log ("Hello, world!");
-// }
-
-for (let i = 10; i > 0 ; i--) {
-  console.log (`second ${i}`)
+for (let index = 0 ; index < 10 ; index++) {
+  console.log ("Hello")
+  console.log ("How are you?")
 }
 
-console.log ("Happy New year")
 
 dayofTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'OneDay']
-               
 
-for (let i = 0; i < dayofTheWeek.length; i++) {
-  console.log (`${dayofTheWeek[i]}`)
-  console.log (`${i}`)
-}
 
 
 
